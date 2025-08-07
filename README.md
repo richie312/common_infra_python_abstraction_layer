3f434564f15da1c692e489f9a1d621e1d94f0f10
richie312 <richie.chatterjee31@gmail.com>
2025-08-07 15:35:26 +0530

    added the kafka produce and consume routes

--- README.md
+++ README.md
@@ -1,36 +1,3 @@
-# Retirement Portfolio Solution
-Retirement Portfolio Solution service plan using kafka, api, redis and backend jobs managed by airflow.
-![alt text](.\images\distributed_system.png)- 
+## Common Infrastructure Python Abstraction Library
 
-## Backend Jobs Managed By Airflow
-
-![alt text](./images/AI_YouTube_Video_Upload.png)
-
-## Timeline
-![alt text](./images/microservice_actions_over_time.png)
-
-
-## Kafka Streaming Service
-
-    a. An api subscribed to kafka topic.
-    b. An api sourcing the kafka producer to produce messages on a topic.
-
-## API
-
-    a. Flask Server
-    b. Has an api for user_interface(Chat bot integration) which takes the messages and source it to the kafka producer.
-    c. Has an api which is listening to subscribed kafka topic.
-    d. Flask-Redis configuration to reply immediately from redis cache server.
-
-## Database
-
-    a. Database for kafka, airflow and redis.
-    b. Database for API, preffeably mongo or cosmos db.
-
-## Container
-
-All of the services are running on containers and final built image is published to the docker hub.
-
-## Kubernetes Deployment
-
-The entire deployment is through kubernetes. Each of the above container will be pulled as an image to deploy pod as service in kubernetes environment.
+This library will be used for multiple applications to interact with common infrastructure like kafka, redis, database and other components.
--- main.py
+++ main.py
@@ -1,45 +1,26 @@
-import requests
-import redis
 import json
 from flask import Flask, request, jsonify
-from kafka_producer import produce_message, create_topic, consume_messages
+from kafka_flask_server.kafka_producer import produce_message, consume_messages
 from confluent_kafka import Consumer, KafkaException, KafkaError
 
 
 app = Flask(__name__)
-cache_map = {}
+
 
 def produce(topic,key,msg):
-    
     if not topic or not msg:
         return jsonify({'error': 'Topic and value are required'}), 400
 
     produce_message(topic, key, msg)
     return jsonify({'status': 'Message produced'}), 200
 
-@app.route('/inject_retirement_params', methods=['POST'])
-def inject_retirement_params():
-    topic = "test-topic"
-    key = "my-key"
+
+@app.route('/produce', methods=['POST'])
+def produce_data():
     incoming_data = json.loads(request.data.decode('utf-8'))
-    # TODO will replace this with redis service
-    if str(incoming_data) in list(cache_map.keys()):
-        value = cache_map[str(incoming_data)]
-        cache_hit = True
-    else:
-        value = requests.get("http://localhost:8000/default-values/")
-        cache_map[str(incoming_data)] = value
-        api_hit = True
-    
-    try:
-        value.status_code == 200
-        value = value.text
-    except AttributeError:
-        return jsonify({'error': 'Failed to fetch default value'}), 500
-    
-    if not topic or not value:
-        return jsonify({'error': 'Topic and value are required'}), 400
-    
+    topic = incoming_data.get('topic')
+    key = incoming_data.get('key')
+    value = incoming_data.get('value')
     # Produce the message to Kafka
     produce(topic, key, value)
     return jsonify({'status': 'Message produced_{}'.format(value)}), 200
@@ -54,4 +35,4 @@ def consume():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    app.run(host = '0.0.0.0',debug=True,port=5001)
+    app.run(host = '0.0.0.0',debug=True,port=5003)
