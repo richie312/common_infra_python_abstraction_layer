83349c4f88ba7babb2741127c0d674f79740241c
Aritra Chatterjee <richie.chatterjee31@gmail.com>
2025-08-16 10:53:40 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,3 +1,4 @@
+import os
 from setuptools import setup, find_packages
 
 def update_octets(version_string):
@@ -20,14 +21,29 @@ def update_octets(version_string):
             octets[1] = 0
             octets[2] = 0
 
-    # Reconstruct the updated version
+    # Reconstruct the updated_version
     updated_version = '.'.join(map(str, octets))
     return updated_version
+
+def write_new_version(version, file_path=".version"):
+    with open(file_path, "w") as f:
+        f.write(version)
+
+def read_last_version(file_path=".version"):
+    if os.path.exists(file_path):
+        with open(file_path, "r") as f:
+            return f.read().strip()
+
+last_updated_version = read_last_version()
+
+current_version = update_octets(last_updated_version)
+
+write_new_version(current_version)
 
 
 setup(
     name="common_infra_python_library",  # Replace with your package name
-    version=update_octets("1.0.1"),  # Replace with your package version
+    version=current_version,  # Replace with your package version
     author="Richie312",  # Replace with your name
     author_email="richie.chatterjee31@gmail.com",  # Replace with your email
     description="Python Library for Common Infrastructure Components like Redis, Kafka, sql, etc",  # Short description
